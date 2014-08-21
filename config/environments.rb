f80d6dc3a9ed7672371d8714f86fc7eaa7c061ff
Nick McDonnough <nickmcd@gmail.com>
2014-08-21 14:52:36 -0500

    fixes

--- config/environments.rb
+++ config/environments.rb
@@ -1,4 +1,10 @@
+require 'active_record'
+require 'active_record_tasks'
+require_relative '../lib/ar.rb'
+
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => 'play'
 )
+
+
---
+++ lib/ar.rb
@@ -0,0 +1,2 @@
+require 'active_record'
+require_relative 'entities/user.rb'
